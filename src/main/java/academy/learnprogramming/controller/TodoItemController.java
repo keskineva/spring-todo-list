6d9481c4d52bcc49eb87770c388a960dcce2300d
keskineva <developer@example.com>
2021-04-19 15:28:15 +0300

    added todoItem serervice , Impl and constructor in controll

---
+++ src/main/java/academy/learnprogramming/controller/TodoItemController.java
@@ -0,0 +1,37 @@
+package academy.learnprogramming.controller;
+
+import academy.learnprogramming.model.TodoData;
+import academy.learnprogramming.service.TodoItemService;
+import academy.learnprogramming.util.Mappings;
+import academy.learnprogramming.util.ViewNames;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@Controller
+public class TodoItemController {
+    //==fields==
+    private final TodoItemService todoItemService;
+
+    //==constructors==
+    @Autowired
+    public TodoItemController(TodoItemService todoItemService) {
+        this.todoItemService = todoItemService;
+    }
+
+    //==model attribute section==
+    @ModelAttribute
+    public TodoData todoData() {
+
+        return todoItemService.getData();
+    }
+
+
+    //==handler methods==
+    //http://localhost:8080/todo-list/items
+    @GetMapping(Mappings.ITEMS)
+    public String items() {
+        return ViewNames.ITEMS_LIST;
+    }
+}
